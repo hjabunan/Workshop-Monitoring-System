cb7136e11bb25c8ec1c098a9b063d07803df7e27
HII-HENRY <hjabunan@gmail.com>
2023-10-06 08:48:23 +0800

    10062023

--- resources/views/workshop-ms/bt-workshop/report.blade.php
+++ resources/views/workshop-ms/bt-workshop/report.blade.php
@@ -2056,8 +2056,8 @@
             </div>
         </div>
         {{-- ADD AND EDIT MODAL FOR PULL OUT UNITS --}}
-        <div id="modalPOU" data-modal-backdrop="static" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-            <div class="relative w-full h-full max-w-4xl max-h-4xl">
+        <div id="modalPOU" data-modal-backdrop="static" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative w-full max-w-4xl max-h-full">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow">
                     <!-- Modal header -->
