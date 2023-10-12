ad5e036c64b3ef0fc9f475f18625999d0f968670
HII-HENRY <hjabunan@gmail.com>
2023-10-12 14:05:54 +0800

    10122023

--- resources/views/system-management/user/index.blade.php
+++ resources/views/system-management/user/index.blade.php
@@ -11,15 +11,17 @@
                         <div class="self-center justify-self-end">
                             <a id="btn-add" href="{{route('user.create')}}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-16 py-2.5 text-center">ADD USER</a>
                         </div>
-                        {{-- <div class=""></div> --}}
-                        <div class="col-span-2 self-center justify-self-end mt-1">
+                    </div>
+                    <div class="px-4 grid grid-cols-4 gap-x-3 ">
+                        <div class="col-span-3 mt-1"></div>
+                        <div class="self-center mt-1"> <!-- This div is added to push the search bar to the right -->
                             <div class="relative">
                                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                     </svg>
                                 </div>
-                                <input type="search" id="searchUser" class="block !w-[100px] p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search User..." required>
+                                <input type="search" id="searchUser" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search User..." required>
                             </div>
                         </div>
                     </div>
